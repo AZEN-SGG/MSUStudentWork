a83a31c0346e7bbcd5473221c1511e7aa85debfc
AZEN-SGG <krivoruchenko.sergei@gmail.com>
2024-11-14 18:36:50 +0300

    Сделал 9 Задание на сортировку

---
+++ Sorting/9Ex/seagwithsob.c
@@ -0,0 +1,33 @@
+#include "seagwithsob.h"
+
+void sort(double * array, int length) {
+       double zero[length];
+       double unit[length];
+       int index;
+       
+       for (int j = 0; j < 63; ++j) {
+	       index = 0;	
+
+	       for (int i = 0; i < length; ++i) {
+		       if ((*(unsigned long long*)&array[i] >> j) & 1) unit[index++] = array[i];
+		       else zero[i - index] = array[i];
+	       }
+
+	       rewrite(array, zero, length - index, unit, index);
+       }
+       
+       index = 0;	
+
+       for (int i = 0; i < length; ++i) {
+	       if ((*(unsigned long long*)&array[i] >> 63) & 1) unit[index++] = array[i];
+	       else zero[i - index] = array[i];
+       }
+
+       for (int i = 0; i < index; ++i) array[i] = unit[index - i - 1];
+       for (int i = index; i < length; ++i) array[i] = zero[i - index];
+}
+
+void rewrite(double * array, double * zero, int len_zero, double * unit, int len_unit) {
+	for (int i = 0; i < len_zero; ++i) array[i] = zero[i];
+	for (int i = len_zero; i < len_zero + len_unit; ++i) array[i] = unit[i - len_zero];
+}
---
+++ Sorting/9Ex/seagwithsob.h
@@ -0,0 +1,9 @@
+#ifndef SEAGWITHSOB
+#define SEAGWITHSOB
+
+#include <string.h>
+
+void sort(double * array, int length);
+void rewrite(double * array, double * zero, int len_zero, double * unit, int len_unit);
+
+#endif
