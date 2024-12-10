b4cb5098c29cdab5835dd7db9952ee8bd54f98df
AZEN-SGG <krivoruchenko.sergei@gmail.com>
2024-12-10 09:19:51 +0300

    Переписал 9 Задачу в одном цикле

--- WorkingArrays/9Ex/grouping.h
+++ WorkingArrays/9Ex/grouping.h
@@ -4,11 +4,9 @@
 #include <stdio.h>
 #include <math.h>
 
-#define eps 1.e-6
+#define exp 1.e-6
 
 void group(double * array, int length);
-void negativeToRight(double * array, int length);
-void zeroesToCenter(double * array, int length);
 void printArray(double * array, int length);
 
 #endif
